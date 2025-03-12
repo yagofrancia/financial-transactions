773f8f7eb9eadda0a1be3c5d705368f84838d837
yagofrancia <yagofrancia@hotmail.com>
2025-03-11 21:24:52 -0300

    refactor: move account and transaction logic to separate services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,53 +1,6 @@
-import {
-  Module,
-  Injectable,
-  Controller,
-  Post,
-  Get,
-  Param,
-  Body,
-  BadRequestException,
-} from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
-import { Account } from './domain/entities/account';
-import { Transaction } from './domain/entities/transaction';
-
-@Injectable()
-export class AccountService {
-  private accounts: Map<string, Account> = new Map();
-  private transactions: Transaction[] = [];
-
-  createAccount(name: string, initialBalance: number): Account {
-    const account: Account = { id: uuid(), name, balance: initialBalance };
-    this.accounts.set(account.id, account);
-    return account;
-  }
-
-  getBalance(accountId: string): number {
-    const account = this.accounts.get(accountId);
-    if (!account) throw new BadRequestException('Conta não encontrada');
-    return account.balance;
-  }
-
-  createTransaction(
-    accountId: string,
-    type: 'entrada' | 'saída',
-    amount: number,
-  ): Transaction {
-    const account = this.accounts.get(accountId);
-    if (!account) throw new BadRequestException('Conta não encontrada');
-
-    if (type === 'saída' && account.balance < amount) {
-      throw new BadRequestException('Saldo insuficiente');
-    }
-
-    const transaction: Transaction = { id: uuid(), accountId, type, amount };
-    this.transactions.push(transaction);
-
-    account.balance += type === 'entrada' ? amount : -amount;
-    return transaction;
-  }
-}
+import { Module, Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { AccountService } from './core/services/account-service';
+import { TransactionController } from './application/transaction-controller';
 
 @Controller('accounts')
 export class AccountController {
@@ -61,27 +14,6 @@ export class AccountController {
   @Get(':id/balance')
   getBalance(@Param('id') id: string) {
     return { balance: this.accountService.getBalance(id) };
-  }
-}
-
-@Controller('transactions')
-export class TransactionController {
-  constructor(private readonly accountService: AccountService) {}
-
-  @Post()
-  createTransaction(
-    @Body()
-    body: {
-      accountId: string;
-      type: 'entrada' | 'saída';
-      amount: number;
-    },
-  ) {
-    return this.accountService.createTransaction(
-      body.accountId,
-      body.type,
-      body.amount,
-    );
   }
 }
 
